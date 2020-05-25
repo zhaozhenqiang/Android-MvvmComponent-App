67ab5e9fcb1352aeddca9139578494d9c0df93ee
jerry <12609480@qq.com>
2020-05-25 20:19:51 +0800

    back some something useless

--- module-user/src/main/java/com/fhzn/db1/user/UserFragment.java
+++ module-user/src/main/java/com/fhzn/db1/user/UserFragment.java
@@ -11,29 +11,18 @@ import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
-import com.blankj.utilcode.util.LogUtils;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.bitmap.CircleCrop;
 import com.bumptech.glide.request.RequestOptions;
-import com.fhzn.common.activity.MvvmBaseActivity;
+import com.fhzn.db1.user.R;
 import com.fhzn.common.fragment.MvvmBaseFragment;
-import com.fhzn.common.fragment.MvvmLazyFragment;
-import com.fhzn.common.net.EasyHttp;
-import com.fhzn.common.net.callback.SimpleCallBack;
-import com.fhzn.common.net.exception.ApiException;
-import com.fhzn.common.router.RouterFragmentPath;
 import com.fhzn.common.viewmodel.IMvvmBaseViewModel;
+import com.fhzn.common.router.RouterFragmentPath;
 import com.fhzn.db1.user.adapter.RecyclerAdapter;
 import com.fhzn.db1.user.databinding.UserFragmentLayoutBinding;
-import com.kingja.loadsir.callback.Callback;
-import com.kingja.loadsir.core.LoadService;
-import com.kingja.loadsir.core.LoadSir;
-import com.scwang.smart.refresh.header.ClassicsHeader;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import io.reactivex.disposables.Disposable;
 
 /**
  * 应用模块:
@@ -45,7 +34,8 @@ import io.reactivex.disposables.Disposable;
  * @since 2020-02-28
  */
 @Route(path = RouterFragmentPath.User.PAGER_USER)
-public class UserFragment extends MvvmBaseFragment<UserFragmentLayoutBinding, IMvvmBaseViewModel> {
+public class UserFragment
+        extends MvvmBaseFragment<UserFragmentLayoutBinding, IMvvmBaseViewModel> {
 
     private RecyclerAdapter adapter;
 
@@ -65,6 +55,7 @@ public class UserFragment extends MvvmBaseFragment<UserFragmentLayoutBinding, IM
         List<String> items = new ArrayList<>();
         items.add("我的关注");
         items.add("我的收藏");
+        //items.add("视频功能声明");
         items.add("用户协议");
         items.add("版权声明");
         items.add("关于复泓");
@@ -89,45 +80,11 @@ public class UserFragment extends MvvmBaseFragment<UserFragmentLayoutBinding, IM
             start(getContext());
         });
         viewDataBinding.tvLike.setOnClickListener(v -> {
-            getNetData("error");
+            start(getContext());
         });
-        viewDataBinding.tvReply.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                getNetData(null);
-            }
+        viewDataBinding.tvReply.setOnClickListener(v -> {
+            start(getContext());
         });
-        viewDataBinding.refreshLayout
-                .setRefreshHeader(new ClassicsHeader(this.getContext()));
-        viewDataBinding.refreshLayout.finishRefresh(true);
-        setLoadSir(viewDataBinding.refreshLayout);
-    }
-
-    private void getNetData(String tag) {
-        showLoading();
-        if (tag == null) {
-            tag = "https://ap2.fuxiang.site/user/getUserInfo";
-        } else {
-            tag = "https://ap2.fuxiang.site/user/getUserInfo22";
-        }
-        Disposable disposable = EasyHttp.post(tag)
-                .formatRequest(null)
-                .cacheKey(getClass().getSimpleName())
-                .execute(new SimpleCallBack<String>() {
-                    @Override
-                    public void onError(ApiException e) {
-                        LogUtils.e(e.getMessage());
-                        //setLoadSir(viewDataBinding.getRoot());
-                        showFailure(e.getMessage());
-                    }
-
-                    @Override
-                    public void onSuccess(String s) {
-                        viewDataBinding.tvTip.setText(s);
-                        showContent();
-                        LogUtils.e(s);
-                    }
-                });
     }
 
     private View getFooterView() {
